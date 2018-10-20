ee30ed1092736bee774d10c0544c592bdfa0b3e4
AlashaqSultanFuckAll <40842773+AlashaqSultanFuckAll@users.noreply.github.com>
2018-10-20 20:50:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,36 +12,36 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    var prefix = "$";
+    var prefix = "!";
 
       if (!message.content.startsWith(prefix)) return;
       var args = message.content.split(' ').slice(1);
       var argresult = args.join(' ');
-      if (message.author.id == 394565174184640524) return;
+      if (message.author.id == 321779633252990976) return;
 
 
     if (message.content.startsWith(prefix + 'playing')) {
-    if (message.author.id !== '394565174184640524') return message.reply('**  لا تلعب ياحبيبي في حساب غيرك**')
+    if (message.author.id !== '321779633252990976') return message.reply('**  لا تلعب ياحبيبي في حساب غيرك**')
     client.user.setGame(argresult);
         message.channel.sendMessage(`**${argresult}** : تم تغيير الحالة`)
     } else
 
 
-    if (message.content.startsWith(prefix + 'streem')) {
-    if (message.author.id !== '394565174184640524') return message.reply('**  لا تلعب ياحبيبي في حساب غيرك **')
+    if (message.content.startsWith(prefix + 'stream')) {
+    if (message.author.id !== '321779633252990976') return message.reply('**  لا تلعب ياحبيبي في حساب غيرك **')
     client.user.setGame(argresult, "http://twitch.tv/fivestore");
         message.channel.sendMessage(`**${argresult}** :تم تغيير الحالة الى ستريمنج`)
     } else
 
     if (message.content.startsWith(prefix + 'setname')) {
-    if (message.author.id !== '394565174184640524') return message.reply('**  لا تلعب ياحبيبي في حساب غيرك **')
+    if (message.author.id !== '321779633252990976') return message.reply('**  لا تلعب ياحبيبي في حساب غيرك **')
       client.user.setUsername(argresult).then
           message.channel.sendMessage(`**${argresult}** : تم تغير الأسم`)
       return message.reply("**لا تستطيع تغير الأسم الا بعد ساعتين**");
     } else
 
     if (message.content.startsWith(prefix + 'setavatar')) {
-    if (message.author.id !== '394565174184640524') return message.reply('** لا تلعب ياحبيبي في حساب غيرك **')
+    if (message.author.id !== '321779633252990976') return message.reply('** لا تلعب ياحبيبي في حساب غيرك **')
     client.user.setAvatar(argresult);
         message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
     }
