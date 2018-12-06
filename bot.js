6150b33accf6f761ce1f8720dae1f90b666c53e0
AlashaqSultanFuckAll <40842773+AlashaqSultanFuckAll@users.noreply.github.com>
2018-12-06 15:33:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 const prefix = "-";
 
 client.on('ready', () => {
-     client.user.setActivity("OverHype Codes 4KK Soon",{type: 'https://www.twitch.tv/OverHype'})
+     client.user.setActivity("OverHype Codes 4KK Soon",{type: 'Streaming'})
 
 });
 
